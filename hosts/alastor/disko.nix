ff75530c9685c0ce947ffc79889f3175115bce25
Jasper Mayone <me@jaspermayone.com>
2025-12-25 20:52:54 -0500

    updates

---
+++ hosts/alastor/disko.nix
@@ -0,0 +1,34 @@
+# Disko configuration for alastor VPS
+# Used by nixos-anywhere for automated partitioning
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
