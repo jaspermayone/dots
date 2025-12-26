428381b629f4347941970603c611b3e78a8b4627
Jasper Mayone <me@jaspermayone.com>
2025-12-26 11:37:04 -0500

    Disable up-arrow binding for atuin

--- modules/shell.nix
+++ modules/shell.nix
@@ -383,6 +383,9 @@ in
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
+    flags = [
+      "--disable-up-arrow"
+    ];
     settings = {
       auto_sync = true;
       sync_frequency = "5m";
